9e5ac7cc6e2b6ab91087ddc74b74149326a8d493
Synth <adlicabulo@gmail.com>
2023-04-15 15:56:45 +0700

    Code Improve

--- run.py
+++ run.py
@@ -1,64 +1,55 @@
-import openai
+import os
 import sys
 import time
-import keyboard
 import threading
 import traceback
-from emoji import demojize
+from dotenv import load_dotenv
+import openai
+import keyboard
 from config import *
 from modules.translate import *
 from modules.TTS import *
 from modules.subtitle import *
 from modules.knowledgeBase import *
 from utils.twitch_config import *
-from dotenv import load_dotenv
 from modules.liveChat import *
 from modules.audioProcess import *
 
 load_dotenv()
 
-# to help the CLI write unicode characters to the terminal
 sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf8', buffering=1)
-
-# use your own API Key, you can get it from https://openai.com/. I place my API Key in a separate file called config.py
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-conversation = []
-# Create a dictionary to hold the message data
-history = {"history": conversation}
+class Conversation:
+    def __init__(self):
+        self.conversation_history = []
+        self.chat_now = ""
+        self.chat_prev = ""
+        self.is_speaking = False
 
-mode = 0
-total_characters = 0
-chat = ""
-chat_now = ""
-chat_prev = ""
-is_Speaking = False
+    def update_chat(self, chat):
+        if not self.is_speaking and chat != self.chat_prev:
+            self.conversation_history.append({'role': 'user', 'content': chat})
+            self.chat_prev = chat
+            return True
+        return False
 
-# function to get the user's input audio
-
-def preparation():
-    global conversation, chat_now, chat, chat_prev
+def prepare_response(conversation):
     while True:
         try:
-            # If the assistant is not speaking, and the chat is not empty, and the chat is not the same as the previous chat
-            # then the assistant will answer the chat
-            chat_now = chat
-            if is_Speaking == False and chat_now != chat_prev:
-                # Saving chat history
-                conversation.append({'role': 'user', 'content': chat_now})
-                chat_prev = chat_now
-                openai_answer()
+            if conversation.update_chat(conversation.chat_now):
+                openai_answer(conversation)
         except Exception as e:
-            # Log the error and continue
-            print(f"Error in preparation(): {e}")
+            print(f"Error in prepare_response(): {e}")
             traceback.print_exc()
         time.sleep(1)
 
-if __name__ == "__main__":
-    t = None
+def main():
+    conversation = Conversation()
+    mode = None
+    thread = None
+
     try:
-        # You can change the mode to 1 if you want to record audio from your microphone
-        # or change the mode to 2 if you want to capture livechat from youtube
         print("Select mode:")
         print(f"{1}: Microphone")
         print(f"{2}: YouTube Live")
@@ -70,22 +61,22 @@ if __name__ == "__main__":
             while True:
                 if keyboard.is_pressed('RIGHT_SHIFT'):
                     record_audio()
-            
+        
         elif mode == "2":
             live_id = input("Livestream ID: ")
-            # Threading is used to capture livechat and answer the chat at the same time
-            t = threading.Thread(target=preparation)
-            t.start()
-            yt_livechat(live_id)
+            thread = threading.Thread(target=prepare_response, args=(conversation,))
+            thread.start()
+            yt_livechat(live_id, conversation)
 
         elif mode == "3":
-            # Threading is used to capture livechat and answer the chat at the same time
             print("To use this mode, make sure to change utils/twitch_config.py to your own config")
-            t = threading.Thread(target=preparation)
-            t.start()
-            twitch_livechat()
+            thread = threading.Thread(target=prepare_response, args=(conversation,))
+            thread.start()
+            twitch_livechat(conversation)
     except KeyboardInterrupt:
-        if t is not None:
-            t.join()
+        if thread is not None:
+            thread.join()
         print("Stopped")
 
+if __name__ == "__main__":
+    main()
